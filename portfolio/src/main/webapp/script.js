8016983fbee27153fe00021336005edcd2304094
Ethan Mooney <ethanimooney@gmail.com>
2020-05-28 16:35:12 -0500

    Addresses all PR comments, ensures the following of Google style guidelines, updates the first minor piece of work in the week 3 curriculum

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
+
 function changeText(text){
     var display = document.getElementById('text-switcher');
     display.innerHTML = "";
