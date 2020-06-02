352fa3e7fa17ad999e27d46974e4c88e284c5a5c
Ethan Mooney <ethanimooney@gmail.com>
2020-06-02 14:44:44 +0000

    Adds beginning functionality for step3, fixes merge conflicts

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> dataList = new ArrayList<String>(Arrays.asList("Hi, I'm Ethan.", "Hey, I'm Ethan!", "Hello! I'm Ethan"));
+ 
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hi, I'm Ethan.");
+  String json = convertToJson(dataList);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(ArrayList<String> dataList) {
+    Gson gson = new Gson();
+    String json = gson.toJson(dataList);
+    return json;
   }
 }
